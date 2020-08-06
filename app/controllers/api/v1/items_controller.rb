7684466d0a5a4effd7165d7a639d533dfaa8d975
github username <github email address>
2020-08-06 14:00:18 -0400

    Created index and create actions, strong_params method for items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::ItemsController < ApplicationController
+    def index
+        @items = Item.all 
+        render json: @items
+    end
+
+    def create
+        @item = Item.create(items_params)
+        if @item.save
+            render json: @item
+    end
+
+
+    def items_params
+        params.require(:item).permit(:item_name, :purchase_price, :date_purchased, :date_sold, :sold)
+    end
 end
+
