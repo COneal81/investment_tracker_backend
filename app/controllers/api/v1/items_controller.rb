b963534304cc7a520ae96da4e7dc7e698e82cc1d
github username <github email address>
2020-08-06 14:22:44 -0400

    for items controller added error message and changed create to new in create action, added show and destroy actions, added before_action and set_item method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,15 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
+    before_action :set_item, only: [:how, :update, :destroy]
+
     def index
         @items = Item.all 
         render json: @items
     end
 
     def create
-        @item = Item.create(items_params)
+        @item = Item.new(items_params)
         if @item.save
             render json: @item
+        else
+            render json: {error: "Item was not saved.  Please try again."}
     end
 
+    def show
+        @item = Item.find(params[:id])
+        render json: @item
+    end
+
+    def destroy
+        @item.destroy
+        render json: {message: "#{@item.item_name} has been removed."}
+    end
+
+    private
+    
+    def set_item
+        @item = Item.find(params[:id])
+    end
 
     def items_params
         params.require(:item).permit(:item_name, :purchase_price, :date_purchased, :date_sold, :sold, :breakeven_point)
